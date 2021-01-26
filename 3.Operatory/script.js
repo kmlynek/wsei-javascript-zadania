76bb61beca69fae595dc40d6566f2bf124cecc4f
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 14:06:39 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,23 @@
+//1
+
+const firstValue = true;
+const secondValue = false;
+
+console.log(firstValue === secondValue);
+
+//2
+
+let first = 6;
+let second = 3;
+let moduloResult = 0;
+
+moduloResult = first % second;
+console.log(moduloResult);
+
+//3
+
+let first = 'first';
+let second = 'second';
+let result = '';
+result = first + second;
+console.log(result);
