4ec64c660d4c46455fdf5e5c2c26a10dcad5e98d
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 16:28:06 +0100

    Update script.js

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -94,3 +94,107 @@ for (let i = 0; i <= 100; i++)
         console.log(i);
       }
   }
+
+//8
+
+//a)
+
+const star = '*'
+for (let i = 0; i < 5; i++) {
+    let result = '';
+    for (let j = 0; j < i; j++) {
+        result += star;
+    }
+    console.log(result)
+}
+
+// b)
+
+for (let i = 1; i <= 5; i++) {
+    
+    const tab = [];
+    for (let j = i; j < 5; j++) {
+      tab.push(" ");
+    }
+    for (let k = 0; k < (2 * i - 1); k++) {
+      const value = k % 2 === 0 ? "*" : " ";
+      tab.push(value);
+    }
+  
+    const row = tab.join("")
+    console.log(row);
+}
+  
+
+
+//c)
+
+for ( let i=1; i<= 5; i++ ){
+    let empty = ' '.repeat( 5-i );
+    let stars = '*'. repeat( 2*i-1 )
+    console.log( empty + stars );
+}
+
+
+//d)
+
+for(let i=1;i<=6;i++){
+    let row = "";
+    for(let j=0; j<i ;j++){
+        row += "*";
+    }
+
+    let number = i;
+    for(let k=6; k>i ;k--){
+        row += number;
+        number ++;
+    }
+    console.log(row);
+    
+}
+console.log("------")
+for(let i=6;i>0;i--){
+   
+    let row = "";
+
+    for(let k=1; k<=i ; k++){
+        row += "*";
+    }
+    let number = i
+    for(let j=6; j>i ; j--){
+      row += number;
+       number++;
+    }
+    console.log(row);
+}
+
+
+
+//e
+
+for (let i = 1; i <= 5; i++) {
+    
+    const tab = [];
+    for (let j = i; j < 5; j++) {
+      tab.push(" ");
+    }
+    for (let k = 0; k < (2 * i - 1); k++) {
+      const value = k % 2 === 0 ? "*" : " ";
+      tab.push(value);
+    }
+  
+    const row = tab.join("")
+    console.log(row);
+}
+
+for ( let i=1; i<3; i++ ){
+
+    const tab = [];
+    for (let j = 0; j < 4; j++) {
+        tab.push(" ");
+    }
+
+    tab.push("*");
+    const row = tab.join("");
+    console.log(row);
+}
