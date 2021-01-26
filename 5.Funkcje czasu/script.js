4cf5b342a5fd3afb94f5919c9e1e102e613348d0
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 16:20:08 +0100

    Update script.js

--- 5.Funkcje czasu/script.js
+++ 5.Funkcje czasu/script.js
@@ -10,3 +10,21 @@ let i =1;
         clearInterval(myTimer);
       };
   }, 3000)
+
+  //2
+
+let myArray = [1, 2, 3, 4];
+let myTimer = setTimeout(() => {
+  for (let i = 0; 1 < myArray.length; i++){
+      Console.log(myArray[i]);
+    }
+}, 2000);
+
+  let j = 0;
+  let myInterval = setInterval(() => {
+    console.log(myArray[j]);
+    j++;
+  if (j >= myArray.length) {
+    clearInterval(myInterval);
+  }
+  }, 3000);
