529c073315d32923135787d3bbb0b29ca71ab777
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 17:12:29 +0100

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -40,3 +40,45 @@ console.log(newArray);
 const newArray =  exampleArray.filter( x => typeof x === "number");
 console.log(newArray);
 
+//3
+
+let exampleArray = [3,5,7,53,378,832];
+
+//3.1
+let counter = 0;
+exampleArray.forEach((elem, i )=>{
+    counter += elem;
+})
+console.log(counter);
+
+//3.2
+let counter1 = 0;
+exampleArray.forEach((elem, i )=>{
+        counter1 -= elem;
+})
+console.log(counter1);
+
+//3.3
+exampleArray.forEach((elem, i )=>{
+        counter += elem;
+})
+console.log(counter / exampleArray.length);
+
+//3.4
+exampleArray.forEach((elem, i) =>{
+    if (elem % 2 == 0) console.log(elem);
+})
+
+//3.5
+exampleArray.forEach((elem, i) =>{
+    if (elem % 2 != 0) console.log(elem);
+})
+
+//3.6
+var max = 0;
+for (let i = 0; i < exampleArray.length; i++){
+    if (max <= exampleArray[i]){
+        max = exampleArray[i];
+    }
+}
+console.log('max: ', max);
