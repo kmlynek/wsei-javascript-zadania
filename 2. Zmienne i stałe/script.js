26f3de85116db507fffde70d922c1c9e760d2d6e
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 13:52:42 +0100

    Update script.js

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -14,3 +14,19 @@ console.log(logic);
 
 let special = null;
 console.log(special);
+
+//2
+
+let firstNumber = 3;
+let secondNumber = 6;
+let sum = 0;
+
+sum = secondNumber + firstNumber;
+console.log(sum);
+
+//3
+
+let nothing;
+console.log(nothing);
+
+//jest wyświetlany wynik undefined, gdyż do zmiennej nie została przypisana wartość
