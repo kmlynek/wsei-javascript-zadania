6778e3ac99bfe587371e0663d27c11673dddef13
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 13:47:01 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,16 @@
+//1
+
+let number = 1;
+console.log(number);
+
+let string = 'string';
+console.log(string);
+
+let numberAndString = 2 + ' dwa';
+console.log(numberAndString);
+
+let logic = true;
+console.log(logic);
+
+let special = null;
+console.log(special);
