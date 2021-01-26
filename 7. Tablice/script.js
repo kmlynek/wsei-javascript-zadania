83555aad1805e3b55dde12a93d04a5cc84df09ff
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 17:03:51 +0100

    Update script.js

---
+++ 7. Tablice/script.js
@@ -0,0 +1,35 @@
+//2
+
+var array = ["One", 2,3,4,5,"Six"];
+
+// 2.1
+console.log(exampleArray[0]);
+console.log(exampleArray[1]);
+
+// 2.2
+
+console.log(exampleArray[exampleArray.length - 1]);
+
+// 2.3
+
+exampleArray.forEach(item => {
+  console.log(item);
+})
+
+// 2.4
+
+const newArray =  exampleArray.filter((x,i) => {
+   return i % 2;
+  })
+console.log(newArray);
+
+// 2.5
+
+const newArray =  exampleArray.filter( x => typeof x === "string");
+console.log(newArray);
+
+// 2.6
+
+const newArray =  exampleArray.filter( x => typeof x === "number");
+console.log(newArray);
+
