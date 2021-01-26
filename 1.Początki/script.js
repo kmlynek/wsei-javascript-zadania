1a03f21f1ccf1f239e74fc6eeccfbae9e1ba1886
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-01-26 13:34:19 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,17 @@
+//1
+
+console.log('Karol')
+
+//2
+
+console.log(12+32)  //wynik 44
+console.log(52/16)  //wynik 3.25
+console.log(5*2)  //wynik 10
+
+//3
+
+console.log("Hello World");
+console.log(Hello World)
+console.log("Hello World)
+
+//Wyświetlony został pierwszy console.log
