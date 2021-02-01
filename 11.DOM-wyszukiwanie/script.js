756d2c43a40d6e8ec36028fbf720c24b83061fc9
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-02-01 20:43:46 +0100

    Update script.js

---
+++ 11.DOM-wyszukiwanie/script.js
@@ -0,0 +1,38 @@
+// 1
+
+const listsElements = document.querySelectorAll(".list");
+console.log(listsElements);
+
+// 2
+
+const getElementByTag = (tagName) => {
+    return document.getElementsByTagName(tagName);
+  }
+  
+  const litsItems = getElementByTag("li");
+  console.log(litsItems);
+
+  
+// 3 
+
+const list = document.querySelector("#list");
+console.log(list);
+
+
+// 4
+
+const displayElement = (element) => {
+    console.log(element);
+}
+
+const liItems = document.querySelectorAll("li");
+const ulItems = document.getElementsByTagName("ul");
+const spansItems = document.getElementsByTagName("span");
+const spansInDiv = document.querySelectorAll("div.list span");
+const spansInDivWithId = document.querySelectorAll("div#spans span");
+
+displayElement(liItems);
+displayElement(ulItems);
+displayElement(spansItems);
+displayElement(spansInDiv);
+displayElement(spansInDivWithId);
