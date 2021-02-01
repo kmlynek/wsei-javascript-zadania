6c79c3798d9217ad741bc35f6f7b89d8b0d4e9e1
kmlynek <61650756+kmlynek@users.noreply.github.com>
2021-02-01 19:49:31 +0100

    Update script.js

--- 9.Konstruktory i prototype/script.js
+++ 9.Konstruktory i prototype/script.js
@@ -65,3 +65,31 @@ let calc2 = new Calculator();
 calc1.multiply(2, 4);
 calc1.multiply(2, 8);
 console.log(calc1.operationHistory);
+
+// 3
+
+function Game(){
+	this.lastNumber = 0;
+	this.number = 0;
+	this.generator = null;
+	
+	this.startGenerator = () => {
+		this.generator = setInterval(() => {
+			this.lastNumber = this.number;
+			this.number = Math.floor((Math.random() * 10) + 1);
+			
+			console.log(this.number);
+			this.checkWin();
+		}, 1000)
+	}
+	
+	this.checkWin = () => {
+		if(this.lastNumber + 5 === this.number){
+			console.log("Wygrałeś");
+			clearInterval(this.generator);
+		}
+	}
+}
+
+let game1 = new Game();
+game1.startGenerator();
